7482b496e84051bfcea17b74a3af6d6366b71358
Anton Klinker <klinker@live.dk>
2020-09-24 13:57:42 +0200

    added an int in another class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         System.out.println("hello world");
 
         int ton = 5;
+        x(5);
+    }
+}
+
+class Ton {
+
+    public static int x (int x) {
+        return x;
     }
 }
