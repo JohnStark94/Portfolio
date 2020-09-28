b2d15b26d5f26579166480e243a374b7ef2030f9
Anton Klinker <klinker@live.dk>
2020-09-28 12:51:53 +0200

    Got started on shapes

---
+++ src/Shape.java
@@ -0,0 +1,9 @@
+public class Shape {
+    protected String type;
+    protected int areal;
+
+    public Shape(String type, int areal) {
+        this.type=type;
+        this.areal=areal;
+    }
+}
