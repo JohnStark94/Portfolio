64ec6f0c3eb0b7cc8d54700e053a49939cedb9f7
jonathanwad <jonathanwad@gmail.com>
2020-11-20 00:08:41 +0100

    Added comments to tests

--- Test/CircleTest.java
+++ Test/CircleTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
+    //create objects to be tested
     Circle circle0 = new Circle("Circle",20,20,20);
     Circle circle1 = new Circle("Circle",25,20,20);
     Circle circle2 = new Circle("Circle",50,20,20);
@@ -10,12 +11,14 @@ class CircleTest {
 
     @Test
     void computeArea () {
+        //create int variables that takes the results from the tested method (in this case compute area)
         int result0 = circle0.computeArea ();
         int result1 = circle1.computeArea ();
         int result2 = circle2.computeArea ();
         int result3 = circle3.computeArea ();
         int result4 = circle4.computeArea ();
 
+        //assertEquals compares an expected result with the actual result that the computeArea method has returned
         assertEquals (1200, result0);
         assertEquals (1875, result1);
         assertEquals (7500, result2);
--- Test/SquareTest.java
+++ Test/SquareTest.java
@@ -1,6 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class SquareTest {
+    //create objects to be tested
     Square square0 = new Square("Square",5,20,20);
     Square square1 = new Square("Square",8,40,40);
     Square square2 = new Square("Square",20,50,50);
@@ -9,12 +10,14 @@ class SquareTest {
 
     @org.junit.jupiter.api.Test
     void computeArea () {
+        //create int variables that takes the results from the tested method (in this case compute area)
         int result0 = square0.computeArea ();
         int result1 = square1.computeArea ();
         int result2 = square2.computeArea ();
         int result3 = square3.computeArea ();
         int result4 = square4.computeArea ();
 
+        //assertEquals compares an expected result with the actual result that the computeArea method has returned
         assertEquals (25, result0);
         assertEquals (64, result1);
         assertEquals (400, result2);
